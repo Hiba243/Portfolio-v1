36218d4495543ef7f49db7ba46c973fd13fa4178
Hiba243 <hibafatima24@gmail.com>
2021-04-24 12:25:13 +0530

    style: finalized jquery animation

--- js/main.js
+++ js/main.js
@@ -25,8 +25,3 @@ function closeMenu() {
     var bodyelem=document.getElementsByTagName('body');
     bodyelem[0].style.position='relative';
 }
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
