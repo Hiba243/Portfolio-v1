c036adb17ed63e6870cd22aef81da120c2b2b951
Hiba243 <hibafatima24@gmail.com>
2021-04-29 21:23:40 +0530

    style: slideshow imgs in landing page

--- js/main.js
+++ js/main.js
@@ -67,4 +67,15 @@ Array.from(links).forEach(link => {
   });
 });
 
+//carousel
+let carousel = document.getElementById("carousel"),
+  slides = carousel.getElementsByTagName("li"),
+  counter = 0,
+  liList = Array.prototype.slice.call(slides);
+
+setInterval(function () {
+  slides[counter].style.opacity = 0; // Hide the previous image
+  counter = (counter + 1) % slides.length; // Increment counter
+  slides[counter].style.opacity = 1; // Show the next image
+}, 3000); // setInterval
 
