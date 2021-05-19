a515d5abe6166c3553f5353428688df956d7b0f5
Hiba243 <hibafatima24@gmail.com>
2021-05-19 11:58:53 +0530

    style: grid outline, scrollbar styling

--- js/main.js
+++ js/main.js
@@ -79,3 +79,12 @@ setInterval(function () {
   slides[counter].style.opacity = 1; // Show the next image
 }, 3000); // setInterval
 
+//cursor
+
+var cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
